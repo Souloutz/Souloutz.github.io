84d00b87f6b1d7bf258fbd63a72659a53fb3c73b
Howard Kong <howardkong826@gmail.com>
2024-12-31 15:55:13 -0500

    Add responsiveness and keyframe animations

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-const clubs = document.getElementsByClassName("clubs");
-const languages = document.getElementsByClassName("languages");
+// const clubs = document.getElementsByClassName("clubs");
+// const languages = document.getElementsByClassName("languages");
 
-// Only button element exists
-clubs[0].addEventListener("click", async function () {
-	this.classList.toggle("clubs-active");
-	var content = this.nextElementSibling;
+// // Only button element exists
+// clubs[0].addEventListener("click", async function () {
+// 	this.classList.toggle("clubs-active");
+// 	var content = this.nextElementSibling;
 
-	if (content.style.maxHeight) {
-		content.style.maxHeight = null;
-	}
+// 	if (content.style.maxHeight) {
+// 		content.style.maxHeight = null;
+// 	}
 
-	else {
-		content.style.maxHeight = content.scrollHeight + "px";
-	}	
-});
+// 	else {
+// 		content.style.maxHeight = content.scrollHeight + "px";
+// 	}	
+// });
 
-// Only button element exists
-languages[0].addEventListener("click", async function () {
-	this.classList.toggle("languages-active");
-	var content = this.nextElementSibling;
+// // Only button element exists
+// languages[0].addEventListener("click", async function () {
+// 	this.classList.toggle("languages-active");
+// 	var content = this.nextElementSibling;
 
-	if (content.style.maxHeight) {
-		content.style.maxHeight = null;
-	}
+// 	if (content.style.maxHeight) {
+// 		content.style.maxHeight = null;
+// 	}
 
-	else {
-		content.style.maxHeight = content.scrollHeight + "px";
-	}
-});
+// 	else {
+// 		content.style.maxHeight = content.scrollHeight + "px";
+// 	}
+// });
